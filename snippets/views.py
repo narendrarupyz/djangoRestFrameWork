b70f93bcde846194b7c9d965005141395992f03f
Narendra Kumar <narendra.kumar@rupyz.com>
2024-04-17 21:47:30 +0530

    Updated, using mixin and genric view

--- snippets/views.py
+++ snippets/views.py
@@ -4,8 +4,11 @@ from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework import mixins
+from rest_framework import generics
 
 # Create your views here.
+#Class based views
 class SnippetList(APIView):
      """
     List all snippets, or create a new snippet.
@@ -49,3 +52,41 @@ class SnippetDetail(APIView):
           snippet = self.get_object(pk)
           snippet.delete()
           return Response(status=status.HTTP_204_NO_CONTENT)
+
+#Using mixins
+class SnippetList(mixins.ListModelMixin,
+                  mixins.CreateModelMixin,
+                  generics.GenericAPIView):
+     queryset = Snippet.objects.all()
+     serializer_class = SnippetSerializer
+
+     def get(self, request, *args, **kwargs):
+          return self.list(request, *args, **kwargs)
+     
+     def post(self, request, **kwargs):
+          return self.create(request, *args, **kwargs)
+
+class SnippetDetail(mixins.RetrieveModelMixin,
+                    mixins.UpdateModelMixin,
+                    mixins.DestroyModelMixin,
+                    generics.GenericAPIView):
+     queryset = Snippet.objects.all()
+     serializer_class = SnippetSerializer
+
+     def get(self, request, *args, **kwargs):
+          return self.retrieve(request, *args, **kwargs)
+     
+     def put(self, request, *args, **kwargs):
+          return self.update(request, *args, **kwargs)
+     
+     def delete(self, request, *args, **kwargs):
+          return self.destroy(request, *args, **kwargs)
+
+#Using generic class-based views
+class SnippetList(generics.ListCreateAPIView):
+     queryset = Snippet.objects.all()
+     serializer_class = SnippetSerializer
+
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
+     queryset = Snippet.objects.all()
+     serializer_class = SnippetSerializer
